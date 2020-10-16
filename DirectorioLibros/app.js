848df9b4814234235e88b30f785c41bfd070f081
danielvera987 <danielveraangulo703@gmail.com>
2020-10-15 20:39:01 -0500

    [Directorio] Configuracion de rutas

--- DirectorioLibros/app.js
+++ DirectorioLibros/app.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const port = process.env.port || 3000;
 
+const libraryRouter = require('./Routers/libros');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', (req, res) => {
-  res.status(200).send("Hola mundo");
-});
+app.use('/v1/api/', libraryRouter);
 
 app.listen(port, () => {
   console.log('app ejecutandose en el puesto 3000');
