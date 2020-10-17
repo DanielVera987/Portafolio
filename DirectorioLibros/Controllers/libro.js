b0f346b13bf55fa8ddbdd8f43cd88f0567d458ab
danielvera987 <danielveraangulo703@gmail.com>
2020-10-16 22:02:11 -0500

    [Directorio] Obtener y crear libros

--- DirectorioLibros/Controllers/libro.js
+++ DirectorioLibros/Controllers/libro.js
@@ -5,12 +5,29 @@ const ModelLibro = require('../Models/Libro');
 const ControllerLibro = {
 
   getLibros: (req, res) => {
-    ModelLibro.create({
-      titulo: 'Spiderman'
-    })
-    .then(x => console.log(x))
+    ModelLibro
+      .find()
+      .exec()
+      .then(x => res.status(200).send(x))
+      .catch(err => res.status(500).send(err))
+  },
 
-    res.status(200).send('Ruta get controllers');
+  create: (req, res) => {
+    const params = req.body;
+
+    if(params.titulo == '' || 
+       params.autor == '' ||
+       params.anio == '' ||
+       params.edicion == '' ||
+       params.paginas == '' || 
+       params.portada == '' || 
+       params.descripcion == ''){
+      return res.status(201).send('Faltan datos');
+    }
+
+    ModelLibro.create(params).then(x => res.status(200).send(x))
+
+
   },
 
 };
--- DirectorioLibros/Routers/libros.js
+++ DirectorioLibros/Routers/libros.js
@@ -4,6 +4,7 @@ const ControllerLibro = require('../Controllers/libro');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', ControllerLibro.getLibros);
+router.get('/libros', ControllerLibro.getLibros);
+router.post('/libros', ControllerLibro.create);
 
 module.exports = router;
